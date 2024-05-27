5c2cd1e90a616001b015bf13340a82ec30189c6c
Mehmet Keçeci <mehmet.42.kececi@gmail.com>
2024-05-27 20:49:07 +0300

    new sector and types added

---
+++ src/main/kotlin/org/secapcompass/secapinventoryapi/domain/building/core/model/OtherTypes.kt
@@ -0,0 +1,6 @@
+package org.secapcompass.secapinventoryapi.domain.building.core.model
+
+enum class OtherTypes{
+    AgricultureForestryFishery,
+    NotAllocated
+}
---
+++ src/main/kotlin/org/secapcompass/secapinventoryapi/domain/building/core/model/TransportType.kt
@@ -0,0 +1,8 @@
+package org.secapcompass.secapinventoryapi.domain.building.core.model
+
+enum class TransportType{
+    MunicipalFleet,
+    PublicTransport,
+    PrivateTransport,
+    NotAllocated
+}
