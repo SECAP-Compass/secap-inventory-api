2e5b48b9b5d6815d4e90371725e5b80df5f2d167
Mehmet Keçeci <mehmet.42.kececi@gmail.com>
2024-06-01 20:27:01 +0300

    Update MeasurementController.kt

--- src/main/kotlin/org/secapcompass/secapinventoryapi/controller/MeasurementController.kt
+++ src/main/kotlin/org/secapcompass/secapinventoryapi/controller/MeasurementController.kt
@@ -4,14 +4,12 @@ import org.secapcompass.secapinventoryapi.domain.building.core.model.BuildingMea
 import org.secapcompass.secapinventoryapi.domain.building.core.model.MeasurementType
 import org.secapcompass.secapinventoryapi.domain.building.core.model.MeasurementTypeHeader
 import org.secapcompass.secapinventoryapi.domain.building.core.repository.IBuildingMeasurementRepository
-import org.secapcompass.secapinventoryapi.domain.building.infrastructure.dto.getMeasurement.GetMeasurementByFilterRequest
+import org.secapcompass.secapinventoryapi.domain.building.core.vo.MeasurementDate
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
-import org.springframework.data.domain.Pageable
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -34,20 +32,29 @@ class MeasurementController(private val buildingMeasurementRepository: IBuilding
         @RequestParam(required = false, defaultValue = "0") page: Int,
         @RequestParam(required = false, defaultValue = "10") size: Int
     ): Page<BuildingMeasurement>{
+        println("here!")
         val pageable = PageRequest.of(page, size)
         return buildingMeasurementRepository.getBuildingMeasurementById(id,pageable)
     }
     @GetMapping("/filter/{buildingId}")
     fun getBuildingMeasurementsByFilter(
         @PathVariable buildingId:UUID,
-        @RequestBody getMeasurementByFilterRequest: GetMeasurementByFilterRequest
+        @RequestParam(required = false) startDate: MeasurementDate?,
+        @RequestParam(required = false) endDate: MeasurementDate?,
+        @RequestParam(required = false) types: List<MeasurementType>?,
+        @RequestParam(required = false) typeHeaders: List<MeasurementTypeHeader>?,
+        @RequestParam(required = false) gasTypes: List<String>?,
+        @RequestParam(required = false, defaultValue = "0") page: Int,
+        @RequestParam(required = false, defaultValue = "10") size: Int
     ): Page<BuildingMeasurement>{
-        val pageable = PageRequest.of(getMeasurementByFilterRequest.page, getMeasurementByFilterRequest.size)
-        return buildingMeasurementRepository.getBuildingMeasurementsByFilter(getMeasurementByFilterRequest.startDate,
-            getMeasurementByFilterRequest.endDate,
-            getMeasurementByFilterRequest.types,
-            getMeasurementByFilterRequest.typeHeaders,
-            getMeasurementByFilterRequest.gasTypes,
+        println("here!\n")
+        val pageable = PageRequest.of(page, size)
+        println("here!\n")
+        return buildingMeasurementRepository.getBuildingMeasurementsByFilter(startDate,
+            endDate,
+            types,
+            typeHeaders,
+            gasTypes,
             pageable
         )
     }
